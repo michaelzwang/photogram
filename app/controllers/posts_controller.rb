5308ee399e0dc5e07cc7ab38550ae66342f11c13
Michael Wang <michaelzwang92@gmail.com>
2016-05-17 15:48:48 -0700

    create flash alerts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.order('created_at DESC')
   end
 
   def show
@@ -13,8 +13,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created! Please check the form."
+      render :new
+    end
   end
 
   def edit
@@ -22,14 +27,17 @@ class PostsController < ApplicationController
 
   def update
     if @post.update(post_params)
+      flash[:success] = "Post updated."
       redirect_to(post_path(@post))
     else
+      flash.now[:alert] = "Update failed. Please check the form."
       render :edit
     end
   end
 
   def destroy
     @post.destroy
+    flash[:success] = "Post deleted."
     redirect_to posts_path
   end
 
