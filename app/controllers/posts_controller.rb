060a9054e07453cc401e9bed630b6d27dd487d27
Michael Wang <michaelzwang92@gmail.com>
2016-05-17 15:31:59 -0700

    refactor by extracting out finding post method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
+  end
+
+  def show
   end
 
   def new
@@ -13,22 +17,18 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
-
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to(post_path(@post))
+    if @post.update(post_params)
+      redirect_to(post_path(@post))
+    else
+      render :edit
+    end
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
@@ -37,6 +37,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
