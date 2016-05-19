68cbe1c770cc7713d3dd461535ac73bc235c9423
Michael Wang <michaelzwang92@gmail.com>
2016-05-19 15:26:50 -0700

    add caption length validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
+  
   validates :user_id, presence: true
   validates :image, presence: true
+  validates :caption, length: { in: 3..300 }
 
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
