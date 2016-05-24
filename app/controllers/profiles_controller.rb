1530aa4503d1bc3a73ca68657542e137aa3eaa77
Michael Wang <michaelzwang92@gmail.com>
2016-05-24 10:42:45 -0700

    edit profile page view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,4 +3,8 @@ class ProfilesController < ApplicationController
     @user = User.find_by(user_name: params[:user_name])
     @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
   end
+
+  def edit
+    @user = User.find_by(user_name: params[:user_name])
+  end
 end
