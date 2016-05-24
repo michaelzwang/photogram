127b4272d8314fe7c3aab6d682148b32110129d1
Michael Wang <michaelzwang92@gmail.com>
2016-05-24 11:00:18 -0700

    authenticate user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,15 +1,19 @@
 class ProfilesController < ApplicationController
+  before_action :owned_profile, only: [:edit, :update]
+  before_action :authenticate_user!
+  before_action :set_user 
+
   def show
-    @user = User.find_by(user_name: params[:user_name])
-    @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
+    @user = set_user
+    @posts = @user.posts.order('created_at DESC')
   end
 
   def edit
-    @user = User.find_by(user_name: params[:user_name])
+    @user = set_user
   end
 
   def update
-    @user = User.find_by(user_name: params[:user_name])
+    @user = set_user
     if @user.update(profile_params)
       flash[:success] = 'Your profile has been updated.'
       redirect_to profile_path(@user.user_name)
@@ -26,5 +30,17 @@ class ProfilesController < ApplicationController
   def profile_params  
     params.require(:user).permit(:avatar, :bio)
   end  
+
+  def owned_profile
+    @user = set_user
+    unless current_user == @user
+      flash[:alert] = "That profile doesn't belong to you!"
+      redirect_to root_path
+    end
+  end
+
+  def set_user  
+    @user = User.find_by(user_name: params[:user_name])
+  end
 
 end
