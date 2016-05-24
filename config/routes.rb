08d42ec1f75151ff9cdfe658daad2c74281e8a12
Michael Wang <michaelzwang92@gmail.com>
2016-05-23 19:49:04 -0700

    add route to user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
   devise_for :users, :controllers => { registrations: 'registrations' }  
   resources :posts do
     resources :comments
   end
 
   root 'posts#index'
+  get ':user_name', to: 'profiles#show', as: :profile 
 end
