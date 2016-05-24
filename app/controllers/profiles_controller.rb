ee422d6039a9f87a4be9cd4c3e1997bf590d0f4c
Michael Wang <michaelzwang92@gmail.com>
2016-05-24 10:34:24 -0700

    edit profile link and route

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
   def show
+    @user = User.find_by(user_name: params[:user_name])
     @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   get 'profiles/show'
 
   devise_for :users, :controllers => { registrations: 'registrations' }  
+  
   resources :posts do
     resources :comments
   end
 
   root 'posts#index'
+
   get ':user_name', to: 'profiles#show', as: :profile 
+  get ':user_name/edit', to: 'profiles#edit', as: :edit_profile  
 end
