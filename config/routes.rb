3257d4eb49133daf2246a5c3b26f7927775fa298
Michael Wang <michaelzwang92@gmail.com>
2016-06-08 10:42:13 -0700

    notification routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,9 @@ Rails.application.routes.draw do
 
   get ':user_name', to: 'profiles#show', as: :profile 
   get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
-  patch ':user_name/edit', to: 'profiles#update', as: :update_profile    
+  patch ':user_name/edit', to: 'profiles#update', as: :update_profile   
+
+  get 'notifications/:id/link_through', to: 'notifications#link_through',
+                                        as: :link_through
+  get 'notifications', to: 'notifications#index'   
 end
