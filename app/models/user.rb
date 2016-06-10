1f890245e9158c7d66b721f5b64d66f3df39318a
Michael Wang <michaelzwang92@gmail.com>
2016-06-10 13:38:08 -0700

    add follow and unfollow methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,12 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, styles: { medium: '152x152#' }  
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  def follow(user_id)  
+    following_relationships.create(following_id: user_id)
+  end
+
+  def unfollow(user_id)
+    following_relationships.find_by(following_id: user_id).destroy
+  end
 end
