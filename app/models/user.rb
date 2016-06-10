e3a7872dafc630ae8a36e681d6a5ea05d6c98d56
Michael Wang <michaelzwang92@gmail.com>
2016-06-10 13:36:43 -0700

    create follow model and set up associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :following, through: :following_relationships, source: :following
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :notifications, dependent: :destroy  
